c5910bc4ab3566c98acee38747ec44a2ac5b66e7
jguillaumesio <62891175+jguillaumesio@users.noreply.github.com>
2020-11-03 18:54:17 +0100

    modidying component from .java

--- src/main/resources/static/vueJS/adress-autocomplete.js
+++ src/main/resources/static/vueJS/adress-autocomplete.js
@@ -1,38 +1,52 @@
-//Script generated with VueComponent at Wed Oct 28 13:40:28 CET 2020
+//Script generated with VueComponent at Tue Nov 03 18:48:10 CET 2020
 Vue.component('adress-autocomplete',{
 	"props":[],"data":function() {
 		 return {
-		 	"timeOut":null,
 			"search":[],"destination":{
 				name:null,loc:null
 				}
-			,"error":false,"selected":""
+			,"error":false,"selected":"","timeOut":null
 			}
 		;
 		}
 	,"methods":{
-		"remove":function (){
-			this.destination={name:null,loc:null};				
-			this.search=[];
-			this.selected="";
-		},
 		"suggestion":function (){
-			let self=this;
-			if(event.target.value.length!=0){
-				if(((event.keyCode >=65 && event.keyCode <= 90) || (event.keyCode >=48 && event.keyCode <= 57) || event.keyCode === 8 || event.keyCode === 27 || event.keyCode === 46 || event.keyCode === 32 )){			
-					var items=event.target.value.split(' ');				
-					let url = "https://api-adresse.data.gouv.fr/search/?q="+items.join("+")+"&limit=8";	
-					clearTimeout(self.timeOut);
-					this.timeOut =setTimeout(function(){
-						fetch(url,{}).then(res => res.json()).then((out) => {self.search=out.features;});
-					}, 500);
-				}
-			}		
-	else{				
-	this.destination={name:null,loc:null};				
+			let self=this;			
+if(event.target.value.length!=0){				
+	if(((event.keyCode >=65 && event.keyCode <= 90) || (event.keyCode >=48 && event.keyCode <= 57) || event.keyCode === 8 || event.keyCode === 27 || event.keyCode === 46 || event.keyCode === 32 )){
+								
+		var items=event.target.value.split(' ');
+									
+		let url = "https://api-adresse.data.gouv.fr/search/?q="+items.join("+")+"&limit=8";
+						
+		clearTimeout(self.timeOut);					
+		this.timeOut =setTimeout(function(){						
+			fetch(url,{
+							}
+						).then(res => res.json()).then((out) => {
+							self.search=out.features;
+							}
+						);
+						}
+					, 500);
+					}
+				}			
+else{				
+	this.destination={
+					name:null,loc:null
+					}
+				;				
 	this.search=[];
-	}
-	}
+				}
+			}
+		,"remove":function (){
+			this.destination={
+				name:null,loc:null
+				}
+			;
+						this.search=[];
+						this.selected='';
+			}
 		}
 	,"template":" <v-autocomplete      v-model=\"selected\"      :items=\"search\"      v-on:keyup=\"suggestion\"      label=\"Select\"      no-filter      hide-details      return-object      item-text=\"properties.name\"      item-value=\"properties.id\"      >      <template v-slot:selection=\"data\">         <v-chip            v-bind=\"data.attrs\"            close            @click=\"data.select\"            @click:close=\"remove(data.item)\"            >            {{ data.item.properties.name }}         </v-chip>      </template>      <template v-slot:item=\"data\">         <v-list-item-content>            <v-list-item-title v-html=\"data.item.properties.name\"></v-list-item-title>            <v-list-item-subtitle v-if=\"data.item.properties.label!=data.item.properties.city\" v-html=\"data.item.properties.city\"></v-list-item-subtitle>            <v-list-item-subtitle v-html=\"data.item.properties.postcode\"></v-list-item-subtitle>         </v-list-item-content>      </template>      </template>   </v-autocomplete>"
 	}
--- src/main/resources/static/vueJS/search-results.js
+++ src/main/resources/static/vueJS/search-results.js
@@ -1,10 +1,10 @@
-//Script generated with VueComponent at Mon Nov 02 16:52:34 CET 2020
+//Script generated with VueComponent at Tue Nov 03 18:48:19 CET 2020
 Vue.component('search-results',{
 	"props":["result"],"data":function() {
 		 return {
-			"map":null,
-			"markers":{},
-			"photo":null
+			"markers":{
+				}
+			,"map":null
 			}
 		;
 		}
@@ -15,14 +15,16 @@ Vue.component('search-results',{
 			window.location.href='/lodgement/'+id+'?'+params;
 			}
 		,"showPopup":function (i){
-			let self=this;
-			console.log(i);
-			var text=i.title.toString()+'<br>prix: '+i.price.toString()+'€<br>Nombre de pièces: '+i.nbr_room+'<br>'+i.nbr_place+' Personne(s)<br>Description:<br>'+i.description;
-			self.markers[i.id].bindPopup(text,{closeButton:false,closeOnEscapeKey:false}).openPopup().addTo(self.map);
+			let self=this;			
+			var text=i.title.toString()+'<br>prix: '+i.price.toString()+'€<br>Nombre de pièces: '+i.nbr_room+'<br>'+i.nbr_place+' Personne(s)<br>Description:<br>'+i.description;			
+			self.markers[i.id].bindPopup(text,{
+				closeButton:false,closeOnEscapeKey:false
+				}
+			).openPopup().addTo(self.map);
 			}
 		,"closePopup":function (i){
 			let self=this;
-				self.markers[i.id].closePopup().addTo(self.map);
+			self.markers[i.id].closePopup().addTo(self.map);
 			}
 		,"getImages":function (){
 			let self = this;
@@ -43,7 +45,9 @@ Vue.component('search-results',{
 							k<response.data.length-1;
 							k++){
 								src={
-									src: '/user-photos/'+self.result[y].rent.id+'/lodgement/'+self.result[y].id+'/'+response.data[k]
+									src: '/user-photos/'+self.
+									$root.result[y].rent.id+'/lodgement/'+self.
+									$root.result[y].id+'/'+response.data[k]
 									}
 								;
 								self.images.push(src);
@@ -62,22 +66,24 @@ Vue.component('search-results',{
 		this.getImages();
 		}
 	,"beforeMount":function(){
-		let self=this;
-					var element = document.getElementById('osm-map');
-					self.map = L.map(element);
-					L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(self.map);
-					var params=new URLSearchParams(window.location.search);
-					var center = L.latLng(params.get('lat'),params.get('lon'));
-					self.map.setView(center, 13);
-					for(i=0;i<self.result.length;i++){
+		let self=this;		
+		var element = document.getElementById('osm-map');		
+		self.map = L.map(element);		
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(self.map);		
+		var params=new URLSearchParams(window.location.search);		
+		var center = L.latLng(params.get('lat'),params.get('lon'));		
+		self.map.setView(center, 13);		
+		for(i=0;
+		i<self.result.length;
+		i++){
 						
-							var text=self.result[i].title.toString()+'<br>prix: '+self.result[i].price.toString()+'€<br>Nombre de pièces: '+self.result[i].nbr_room+'<br>'+self.result[i].nbr_place+' Personne(s)<br>Description:<br>'+self.result[i].description;
-							center=L.latLng(self.result[i].lat,self.result[i].lon);
-							var marker=L.marker(center);
-							self.markers[self.result[i].id]=marker;
-							marker.bindPopup(text).openPopup().addTo(self.map);
-					}
+			var text=self.result[i].title.toString()+'<br>prix: '+self.result[i].price.toString()+'€<br>Nombre de pièces: '+self.result[i].nbr_room+'<br>'+self.result[i].nbr_place+' Personne(s)<br>Description:<br>'+self.result[i].description;			
+			center=L.latLng(self.result[i].lat,self.result[i].lon);			
+			var marker=L.marker(center);			
+			self.markers[self.result[i].id]=marker;			
+			marker.bindPopup(text).openPopup().addTo(self.map);
+			}
 		}
-	,"template":"<v-data-iterator               :items=\"result\"               hide-default-footer               >               <v-row                  class=\"mb-6\"                  no-gutters                  >                  <v-card                     v-for=\"i in result\" :key=\"i.id\"                     class=\"mx-auto\"                     elevation=\"0\"                     style=\"margin:0!important;width:100%\"                     align=\"top\"                     @mouseenter.native=\"showPopup(i)\"                     @mouseleave.native=\"closePopup(i)\"                     >                     <v-list-item two-line>                           <v-col                              cols=\"4\"                              style=\"padding:0;\"                              >                            <v-list-item-content >                              <v-carousel style=\"border-radius:5px;width:300px;\"hide-delimiters height=\"200\" >                                 <v-carousel-item                                    v-for=\"item in i.images\"                                    :src=\"item.src\"                                    ></v-carousel-item>                              </v-carousel>                             </v-list-item-content>                           </v-col>                           <v-col                              cols=\"8\"                              @click=\"redirect(i.id)\"                              style=\"cursor: pointer;\"                                                            >                               <v-list-item-content>                              <v-list-item-title class=\"headline mb-1\">Description de la maison</v-list-item-title>                              <div class=\"overline mb-4\">{{i.type}}</div>                              <v-list-item-subtitle>Nbr de voyageurs - Nbr de pièces</v-list-item-subtitle>                              <v-list-item-subtitle>Liste des équipements</v-list-item-subtitle>                           </v-list-item-content>                           </v-col>                     </v-list-item>                     <v-divider></v-divider>                  </v-card>               </v-row>            </v-data-iterator>"
+	,"template":"<v-data-iterator               :items=\"result\"               hide-default-footer>   <v-row                     class=\"mb-6\"                     no-gutters>      <v-card                           v-for=\"i in result\" c:key=\"i.id\"                           class=\"mx-auto\"                           elevation=\"0\"                           style=\"margin:0!important;width:100%\"                      align=\"top\"                          @mouseenter.native=\"showPopup(i)\"                           @mouseleave.native=\"closePopup(i)\">                           <v-list-item two-line>         <v-col                                       cols=\"4\"                                       style=\"padding:0;\">            <v-list-item-content >               <v-carousel               style=\"border-radius:5px;width:300px;\"               hide-delimiters height=\"200\">                  <v-carousel-item                                                      v-for=\"item in i.images\"                                                      :src=\"item.src\"></v-carousel-item>               </v-carousel>            </v-list-item-content>         </v-col>         <v-col                                       cols=\"8\"                               @click=\"redirect(i.id)\"                                       style=\"cursor: pointer;\">                                        <v-list-item-content>            <v-list-item-title class=\"headline mb-1\">Description de la maison</v-list-item-title>            <div class=\"overline mb-4\">{{i.type}}</div>            <v-list-item-subtitle>Nbr de voyageurs - Nbr de pièces</v-list-item-subtitle>            <v-list-item-subtitle>Liste des équipements</v-list-item-subtitle>         </v-list-item-content>         </v-col>                           </v-list-item>      <v-divider></v-divider>      </v-card>                  </v-row></v-data-iterator>"
 	}
 );
